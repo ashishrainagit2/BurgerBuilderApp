19917ab114be408c9146b265f09fde15febccfcd
Ashish Raina <ashish.raina@bookmyshow.com>
2019-05-06 08:37:17 +0530

    burger builder

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,62 @@
+import React  , {Component } from 'react';
+import Aux from '../../hoc/Aux';
+import Burger  from '../../components/Burger/Burger' ;
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese : 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+class BurgerBuilder extends Component{
+    // constuctor(props){
+    //     super(props);
+    //     this.state = { ... }
+    // }
+    state = {
+        ingredients : {
+            salad : 0,
+            bacon : 0,
+            cheese : 3,
+            meat : 1
+        },
+        totalPrice : 4
+    }
+
+    addIngredientHandler = (type) => {
+       const oldCount = this.state.ingredients[type];
+       const updatedCount = oldCount + 1 ;
+       const updatedIngredients = {
+           ...this.state.ingredients
+       };
+       updatedIngredients[type] = updatedCount;
+       const priceAddition =  INGREDIENT_PRICES[type];
+       const oldPrice  = this.state.totalPrice;
+       const newPrice = oldPrice + priceAddition;
+       this.setState({
+           totalPrice: newPrice , 
+           ingredients : updatedIngredients 
+       })
+
+
+    }
+
+    removeIngredientHandler = (type) => {
+
+    }
+
+
+
+    render(){
+        return(
+            <Aux>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}/>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder ;
